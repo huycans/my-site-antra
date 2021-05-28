65c1d23cd36e0ec743eb263f9c3b67730d59b5a3
huycans <huycan_lk@yahoo.com.vn>
2021-05-28 14:44:36 -0400

    Add routing for three main pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,27 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
 import "./App.css";
+import Contact from "./components/Contact";
+import Home from "./components/Home";
+import Portfolio from "./components/Portfolio";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  return <div className="App">APP</div>;
+  return (
+    <div className="App">
+      <Switch>
+        <Route exact path="/Contact">
+          <Contact></Contact>
+        </Route>
+        <Route exact path="/portfolio">
+          <Portfolio></Portfolio>
+        </Route>
+        <Route path="/">
+          <Home></Home>
+        </Route>
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
