b9658c251220678d2be62eef88b7681f20bf9010
huycans <huycan_lk@yahoo.com.vn>
2021-05-28 17:46:42 -0400

    Responsiveness for navbar

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -7,8 +7,8 @@ export default function Nav() {
   return (
     <header className="navigation">
       <div className="container-fluid">
-        <div className="row">
-          <div className="col-xs-1 col-sm-1 col-md-1 col-lg-1 text-center">
+        <div className="nav-items row">
+          <div className="logo-div col-xs-1 col-sm-1 col-md-1 col-lg-1 text-center">
             <div className="logo">
               <Link to="/">
                 <img src={logo} />
@@ -16,19 +16,17 @@ export default function Nav() {
             </div>
           </div>
 
-          <div className="col-xs-11 col-sm-11 col-md-11 col-lg-11 text-right">
+          <div className="nav-links col-xs-11 col-sm-11 col-md-11 col-lg-11 text-right">
             <div className="primary-nav">
-              <ul>
-                <li>
-                  <Link to="/">Home</Link>
-                </li>
-                <li>
-                  <Link to="portfolio">Portfolio</Link>
-                </li>
-                <li>
-                  <Link to="contact">Contact</Link>
-                </li>
-              </ul>
+              <div>
+                <Link to="/">Home</Link>
+              </div>
+              <div>
+                <Link to="portfolio">Portfolio</Link>
+              </div>
+              <div>
+                <Link to="contact">Contact</Link>
+              </div>
             </div>
           </div>
         </div>
